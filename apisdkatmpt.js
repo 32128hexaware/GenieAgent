0fb86f934226e5474b7bc4efd254924fc14be049
Jeyam pandi Bose (jp) <32128@hexaware.com>
2017-07-12 11:55:40 +0530

    'test'

--- apisdkatmpt.js
+++ apisdkatmpt.js
@@ -1,8 +1,10 @@
 var apiai = require('apiai');
-var prompt = require('prompt')
+var prompt = require('prompt');
+var bodyParser = require('body-parser');
 var access_token = "e0953be2010e4d47b3b624dd3747d4f2";
 var app = apiai(access_token);
-
+//app.use(bodyParser.urlencoded({extended:true}));
+//app.use(bodyParser.json());
 
 run_main('hi');
 
@@ -17,7 +19,7 @@ function run_main(query1) {
 	//console.log(request);
 	request.on('response', function(response) {
 
-      console.log(JSON.stringify(response.body));
+      console.log(response);
 
 	});
 
